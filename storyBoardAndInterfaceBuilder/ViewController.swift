12b4e60a204c85e006b9b5f67d698b94cc7c67cb
latifulfikri <latifulfikri1902@gmail.com>
2023-03-02 12:17:48 +0700

    xcode project setup

---
+++ storyBoardAndInterfaceBuilder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyBoardAndInterfaceBuilder
+//
+//  Created by Fikri Yuwi on 02/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ storyBoardAndInterfaceBuilderUITests/storyBoardAndInterfaceBuilderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  storyBoardAndInterfaceBuilderUITestsLaunchTests.swift
+//  storyBoardAndInterfaceBuilderUITests
+//
+//  Created by Fikri Yuwi on 02/03/23.
+//
+
+import XCTest
+
+final class storyBoardAndInterfaceBuilderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
